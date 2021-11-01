f51327f605b6a5c8e6db23621cc1c5b64681740d
NANDI GUO <907300985@qq.com>
2021-11-01 17:10:47 +0800

    update

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = calulate(1, 2);
+            Console.WriteLine(sum);
+            Animal cat = new Animal();
+            cat.eat();
+            cat.fly();
+     
+        }
+
+        static int calulate(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+
+    public interface fly
+    {
+        void fly();
+    }
+
+    public class Animal: Program, fly{
+        
+        public Animal()
+        {
+            Console.WriteLine("小动物吃都行");
+        }
+
+       
+        public void eat()
+        {
+            Console.WriteLine("小动物");
+        }
+
+   
+        public void fly()
+        {
+            Console.WriteLine("我会飞");
+        }
+    }
+}
