9f60ce34bced4f6cf799d76cdb4874db2d3794af
NANDI GUO <907300985@qq.com>
2021-11-01 19:55:39 +0800

    对象

---
+++ ClassAndInstance1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassAndInstance1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form myform = new Form();
+          
+            //Form myform2;
+            //myform2 = myform;
+            //Console.WriteLine(myform2 == myform);
+            myform.Text = "啥也不是";
+            myform.ShowDialog();
+       }
+    }
+}
