76698528395cd520fec4d3012eb08c830c98cb33
NANDI GUO <907300985@qq.com>
2021-11-01 21:40:05 +0800

    recursion

---
+++ MyExample/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Calculation.Method1(100);
+            int result2 = Calculation.MySum(100);
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Calculation.Hnoi(3, "A", "B", "C");
+            Form form = new Form();
+            form.ShowDialog();
+        }
+    }
+
+    class Calculation
+    {
+        public static int Method1(int sum)
+        {
+            int s = 0;
+            for (int i = 1; i <= sum; i++)
+            {
+                s += i;
+            }
+            return s;
+        }
+
+        public static int MySum(int x)
+        {
+            if (x == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                int result = x + MySum(x - 1);
+                return result;
+            }
+        }
+        public static void Hnoi(int s, String from, String mid, String to)
+        {
+            if(s == 1)
+            {
+                Console.WriteLine(from +" move to "+ to);
+            }
+            else
+            {
+                Hnoi(s-1,from,to,mid);
+                Console.WriteLine(from+" move to "+to);
+                Hnoi(s - 1, mid, from, to);
+            }
+        }
+    }
+}
---
+++ StaticSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StaticSample
+{
+    class Program
+
+    {
+        static String name = "NANDI";
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("静态方法");
+            Form form = new Form();
+            form.Text = "hello";
+            form.ShowDialog();
+        }
+        public void eat()
+        {
+            Console.WriteLine("eat");
+        }
+    }
+}
