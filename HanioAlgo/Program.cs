4d81e1462a491797a73c10a12b66f484bd7d49da
NANDI GUO <907300985@qq.com>
2021-11-02 14:23:09 +0800

    Hanoi

---
+++ HanioAlgo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HanioAlgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyHano(2, "A", "B", "C");
+        }
+
+        public static void MyHano(int s, String left, String mid, String right)
+        {
+            if(s == 1)
+            {
+                Console.WriteLine(left+" 移到 "+right);
+            }
+            else
+            {
+                MyHano(s-1,left, right, mid);
+                Console.WriteLine(left+" 移到 "+right);
+                MyHano(s - 1, mid, left, right);
+            }
+        }
+
+    }
+}
