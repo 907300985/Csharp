47e468da519b79ca351dbad9f79762ca9cea5673
NANDI GUO <907300985@qq.com>
2021-11-03 21:03:22 +0800

    属性

---
+++ getset/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace getset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People p = new People();
+            p.h = 10;
+            p.Age = 100;
+            Console.WriteLine(p.h);
+            Console.WriteLine(People.love);
+
+        }
+    }
+    class People
+    {
+ 
+        public int h{ get; set; }
+        public int w{ get; set; }
+        public int Age { get => age; set => age = value; }
+
+        private int age;
+        public  const int love = 3000;
+        
+    }
+}
---
+++ trycatch/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace trycatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(new Cal().Add("99999999999999", "123")); 
+
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            /*            foreach (int l in list)
+                        {
+                            Console.WriteLine(l);
+                        }*/
+            IEnumerator e = list.GetEnumerator();
+            while (e.MoveNext())
+            {
+                Console.WriteLine(e.Current);
+            }
+
+
+          
+
+        }
+
+    }
+    class Cal
+    {
+        public int Add(String arg1, String arg2)
+        {
+            int a = 0, b = 0;
+            
+            try
+            {
+                a = int.Parse(arg1);
+                b = int.Parse(arg2);
+            }
+            catch(ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+            catch(FormatException fe)
+            {
+                Console.WriteLine(fe.Message);
+            }
+            catch(OverflowException oe)
+            {
+                Console.WriteLine(oe.Message);
+       
+            }
+            return a + b;
+        }
+    }
+}
