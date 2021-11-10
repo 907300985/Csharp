155252b2f12698f52618b413a1a3d7d340200280
NANDI GUO <907300985@qq.com>
2021-11-10 10:16:37 +0800

    css

---
+++ EventExample/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace EventExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            Waiter waiter = new Waiter();
+        }
+    }
+    public class OrderEventArgs
+    {
+        public string DishName { get; set; }
+        public string Size { get; set; }
+    }
+
+    public delegate void OrderEventHandler(Customer customer, OrderEventArgs e);
+
+    public class Customer
+    {
+        private OrderEventHandler orderEventHandler;
+
+        public event OrderEventHandler Order
+        {
+            add
+            {
+                this.orderEventHandler += value;
+            }
+            remove
+            {
+                this.orderEventHandler -= value;
+            }
+        }
+        
+        public int Bill { get; set; }
+
+        public void PayTheBill()
+        {
+            Console.WriteLine("I will pay $" + this.Bill);
+        }
+    }
+
+    public class waiter
+    {
+        public void Action(Customer customer, OrderEventArgs e)
+        {
+            Console.WriteLine("I will serve you the dish -{0}",e.DishName);
+            double price = 10;
+            switch (e.Size)
+            {
+                case "small":
+                    price *= 0.5;
+                    break;
+            }
+        }
+        Customer.Bill += price;
+    }
+}
---
+++ TestOverride/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TestOverride
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v1 = new RaceCar();
+            Vehicle v = new Car();
+
+            Action action = new Action(v1.Run);
+            //action += v.Run;
+            action.Invoke();
+
+        }
+    }
+    class Vehicle { 
+        public virtual void Run()
+        {
+            Console.WriteLine("父类我在跑");
+        }
+    }
+    class Car: Vehicle
+    {
+        //public int Speed { get; set; }
+        public override void Run()
+        {
+            //base.Run();
+            Console.WriteLine("重写后的子类再跑");
+        }
+    }
+    class RaceCar: Car
+    {
+        public override void Run()
+        {
+            Console.WriteLine("赛车正在跑");
+        }
+    }
+}
